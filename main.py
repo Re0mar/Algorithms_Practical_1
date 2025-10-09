8c7cf8e236b07263536e134dee3f79eaf4950500
Re0mar <daniel.groenendijk.dg@protonmail.com>
2025-10-09 14:15:20 +0200

    provided actual adj list

--- main.py
+++ main.py
@@ -196,5 +196,15 @@ if __name__ == '__main__':
         [1, 2] # 4
     ]
 
+    mockAdjListActual = [
+        [[1, 2],[4, 2]], # 0
+        [[0, 2],[4, 2]], # 1
+        [[3, 2],[4, 2]], # 2
+        [[1, 2],[4, 2]], # 3
+        [[2, 2],[1, 2]] # 4
+    ]
+
     PrimMST(mockAdjList)
 
+    print(mockAdjList)
+    print(road_adj_list)
